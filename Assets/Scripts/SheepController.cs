13ab3f9cd7b5975b2d08f33b473236f9654feee4
sqrt(1-cos**2) <114472721+hawng289@users.noreply.github.com>
2023-09-26 17:20:32 +0700

    di chuyển con cừu theo ý muốn

--- Assets/Scripts/SheepController.cs
+++ Assets/Scripts/SheepController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class SheepController : MonoBehaviour
 {
+    public float moveSpeed;
+    Vector3 mousePos;
+    float minX = -4.7f;
+    float maxX = 4.7f;
+    float minY = -2.6f;
+    float maxY = 2.6f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,13 @@ public class SheepController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos = new Vector3(Mathf.Clamp(mousePos.x, minX, maxX), Mathf.Clamp(mousePos.y, minY, maxY), 0);
+           
+        }
+        transform.position = Vector3.Lerp(transform.position, mousePos, moveSpeed * Time.deltaTime);
+
     }
 }
